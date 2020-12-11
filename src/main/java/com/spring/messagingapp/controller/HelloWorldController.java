2087c92add340ed570ac034ea3743b2a00f1c302
Ard2238 <abhishekrdas28@gmail.com>
2020-12-11 18:53:39 +0530

    Added Code to add a hello controller and use get request method

--- src/main/java/com/spring/messagingapp/controller/HelloWorldController.java
+++ src/main/java/com/spring/messagingapp/controller/HelloWorldController.java
@@ -3,7 +3,13 @@ package com.spring.messagingapp.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
 
+    // GET Request :- Rest call: curl localhost:8080/hello -w "\n"
+    @RequestMapping(value = {"", "/", "/home"})
+    public String hello(){
+        return "Hello from Bridgelabz";
+    }
 }
